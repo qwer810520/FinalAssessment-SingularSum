94b368f9f730a2a464d448d0e02c18bee4996c2a
楷岷 張 <MIN@Min-2.local>
2017-05-12 14:36:50 +0800

    第一題完成

---
+++ 第一題.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+func finalAssessment() -> Int {
+    var x = 0
+    for i in 1...100 {
+        if i % 2 != 0 {
+            x += i
+        }
+    }
+    return x
+}
+
+
+finalAssessment()
